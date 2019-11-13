bac059bc3fc98d7e340eabb9ee4dea479feb0196
Saim Zahid <saimzahid@me.com>
2019-11-13 14:51:35 -0500

    Initial Commit

---
+++ Wattpad Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wattpad Assessment
+//
+//  Created by Saim Zahid on 2019-11-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
