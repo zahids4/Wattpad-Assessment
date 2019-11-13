7453fec76180147639a412b9e48d8579b01b777c
Saim Zahid <saimzahid@me.com>
2019-11-13 18:26:23 -0500

    Fetching and presenting stories on tableview

--- Wattpad Assessment/Models/StoriesJSON.swift
+++ Wattpad Assessment/Models/StoriesJSON.swift
@@ -18,4 +18,8 @@ public struct StoriesJSON: Decodable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         stories = try container.decode([Story].self, forKey: .stories)
     }
+    
+    func getStoryViewModels() -> [StoryViewModel] {
+        return stories.map({ StoryViewModel($0) })
+    }
 }
